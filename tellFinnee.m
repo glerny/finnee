06e81d8598806d0b706374b940c078dca4967aa5
glerny <guillaume@fe.up.pt>
2016-01-13 10:45:50 +0000

    created tellFinnee

---
+++ tellFinnee.m
@@ -0,0 +1,24 @@
+function tellFinnee(finneeStc)
+%% DESCRIPTION
+% 1. INTRODUCTION
+% TELLFINNEE gives informations about the input finneeStc.
+%
+% 2. PARAMETERS:
+%   .required. TELLFINNEE requires at least 1 parameter
+%       finneeStc
+%           is the finnee structure that contain information about the run
+%           and link and indexation of the associated dat file. The
+%           strcuture should have been create by function such as 
+%           DOMZML2STRUCT
+%
+% 3. EXAMPLES:
+%	tellFinnee(finneeStc)
+%
+% 4. COPYRIGHT
+% Copyright 2015-2016 G. Erny (guillaume@fe.up.pt), FEUP, Porto, Portugal
+%
+
+%% CORE OF THE FUNCTION
+% 1. General 
+end
+
